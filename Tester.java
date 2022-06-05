96ebe2a8cc3d45e2f7d21f87e47ae24ee14a62a1
Emmanuel Soon <reach.emmanuelsoon@gmail.com>
2022-06-05 13:05:17 +0800

    Week 3 - Filters & hashmaps

--- Tester.java
+++ Tester.java
@@ -6,9 +6,18 @@ public class Tester {
         // fr.testLoadRaters();
 
         // Second Assignment
-        MovieRunnerAverage.printAverageRatings();
+        // MovieRunnerAverage.printAverageRatings();
         //MovieRunnerAverage.getAverageRatingOneMovie();
 
+
+        // third Assignment 
+        // MovieRunnerWithFilters.printAverageRatings();
+        // MovieRunnerWithFilters.getAverageRatingByYear(20, 2000);
+        // MovieRunnerWithFilters.getAverageRatingByGenre(20, "Comedy");
+        // MovieRunnerWithFilters.getAverageRatingByMinutes(5, 105, 135);
+        // MovieRunnerWithFilters.getAverageRatingByDirectors(4, "Clint Eastwood,Joel Coen,Martin Scorsese,Roman Polanski,Nora Ephron,Ridley Scott,Sydney Pollack");
+        // MovieRunnerWithFilters.getAverageRatingByYearAFterAndGenre(8, 1990, "Drama");
+        // MovieRunnerWithFilters.getAverageRatingByDirectorsAndMinutes(3, "Clint Eastwood,Joel Coen,Tim Burton,Ron Howard,Nora Ephron,Sydney Pollack", 90, 180);
     }
     
 }
